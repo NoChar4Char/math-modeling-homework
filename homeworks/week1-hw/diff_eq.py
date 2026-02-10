f18e6c20d67f4a2ca6c31c60f02257c6b60c06d1
Charlie Wang <hwbrother1010@gmail.com>
2026-02-09 17:22:15 -0800

    got the correct error v. timestep graphs, but DT=0.001 still fails

--- homeworks/week1-hw/diff_eq.py
+++ homeworks/week1-hw/diff_eq.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class DiffEq:
-    def __init__(self, f:function, T_FINAL:int, DT:float, Y_0:float, ACTUAL_Y):
+    def __init__(self, f:function, T_FINAL:int, DT:float, Y_0:float, ACTUAL_Y): # broken if DT not divisible by T_FINAL
         self.dimensions = None
         try:
             self.dimensions = ACTUAL_Y.shape[1]
@@ -13,9 +13,10 @@ class DiffEq:
         self.DT = DT
         self.Y_0 = Y_0
         self.ACTUAL_Y = ACTUAL_Y
-        self.t_vals = np.linspace(0, T_FINAL, int(T_FINAL/DT)+1)
+        if T_FINAL/DT != int(T_FINAL/DT):
+            raise ValueError(f"DT={DT} doesn't divide into T_FINAL={T_FINAL}, remainder is {int(T_FINAL/DT)}")
+        self.t_vals = np.linspace(0, T_FINAL, int(T_FINAL/DT) + 1)
         self.y_vals = np.zeros((int(T_FINAL/DT)+1)) if self.dimensions == 1 else np.zeros((int(T_FINAL/DT)+1, self.dimensions))
-        # self.y_vals = np.zeros((int(T_FINAL/DT)+1, self.dimensions))
         self.is_filled = False
         self.labels = dict(
             fontsize=10,
@@ -34,7 +35,7 @@ class DiffEq:
         for i in range(n_steps):
             self.y_vals[i+1] = self.y_vals[i]+self.DT * self.f(self.t_vals[i+1], self.y_vals[i])
         self.is_filled = True
-        return self.y_vals[-1]
+        # return self.y_vals[-1]
 
     def explicit_midpoint(self):
         n_steps = int(self.T_FINAL/self.DT)
@@ -43,7 +44,7 @@ class DiffEq:
             y_mid = self.y_vals[i]+self.DT/2 * self.f(self.t_vals[i]+self.DT/2, self.y_vals[i])
             self.y_vals[i+1] = self.y_vals[i]+self.DT * self.f(self.t_vals[i+1], y_mid)
         self.is_filled = True
-        return self.y_vals[-1]
+        # return self.y_vals[-1]
 
     def rk4(self):
         n_steps = int(self.T_FINAL/self.DT)
@@ -54,7 +55,7 @@ class DiffEq:
             y3 = self.y_vals[i]+self.DT/2 * self.f(self.t_vals[i]+self.DT/2, y2)
             self.y_vals[i+1] = self.y_vals[i]+self.DT * self.f(self.t_vals[i+1], y3)
         self.is_filled = True
-        return self.y_vals[-1]
+        # return self.y_vals[-1]
     
     def plot(self, color:str, is_loglog:bool, is_diff:bool, label_val:str) -> None:
         if (not self.is_filled):
@@ -91,3 +92,32 @@ class DiffEq:
 
         plt.plot(self.y_vals[:, 0], self.y_vals[:, 1], c=color, **self.line_style)
         plt.show()
+
+    def plot_error(self, color_list:list, time_list:list, method_list:list) -> None: # actual task1
+        original_DT = self.DT
+        for i in range(len(method_list)):
+            points = np.zeros(len(time_list)) if self.dimensions == 1 else np.zeros(len(time_list), self.dimensions)
+            for j in range(len(time_list)):
+                self.DT = time_list[j]
+                self.t_vals = np.linspace(0, self.T_FINAL, int(self.T_FINAL/self.DT) + 1)
+                self.y_vals = np.zeros((int(self.T_FINAL/self.DT)+1)) if self.dimensions == 1 else np.zeros((int(self.T_FINAL/self.DT)+1, self.dimensions))
+                if self.T_FINAL/self.DT != int(self.T_FINAL/self.DT):
+                    raise ValueError(f"DT={self.DT} doesn't divide into T_FINAL={self.T_FINAL}")
+                match method_list[i]:
+                    case "Forward Euler" | "1":
+                        self.forward_euler()
+                    case "Explicit Midpoint" | "2":
+                        self.explicit_midpoint()
+                    case "Low Storage Runge-Kutta 4" | "3":
+                        self.rk4()
+                    case _:
+                        print("???")
+                        continue
+                
+                points[j] = np.abs(self.y_vals[-1] - self.ACTUAL_Y[-1]) if self.dimensions == 1 else np.abs(self.y_vals[-1][0] - self.ACTUAL_Y[-1][0])
+            plt.loglog(time_list, points, c=color_list[i], label=method_list[i], **self.line_style)
+        plt.legend()
+        plt.xlabel("Timestep", **self.labels)
+        plt.ylabel("Error", **self.labels)
+        plt.show()
+        self.DT = original_DT
